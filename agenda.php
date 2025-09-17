48ff2aefe5f2a0ab89cd0053a5795d63afa966e5
AleKrasniak <142438661+AleKrasniak@users.noreply.github.com>
2025-09-17 09:18:29 -0300

    Update agenda.php

--- agenda.php
+++ agenda.php
@@ -6,7 +6,6 @@ ini_set('display_errors', 1);
 include 'inc/header.inc.php';
 include 'classes/contatos.class.php';
 include 'classes/funcoes.class.php';
-include 'classes/usuario.class.php';
 
 if (!isset($_SESSION['usuario'])) {
     header('Location: login.php');
@@ -15,19 +14,14 @@ if (!isset($_SESSION['usuario'])) {
 
 $usuario = $_SESSION['usuario'];
 $contato = new Contatos();
-$fn = new funcoes();
-
+$fn = new Funcoes();
 ?>
 
 <h1>Agenda Senac 2025</h1>
 
 <p>Bem-vindo, <?= htmlspecialchars($usuario['nome']) ?>! <a href="logout.php">Sair</a></p>
 
-<?php if (Usuario::temPermissao($usuario, 'adm')): ?>
-    <p><a href="usuarios.php"><button>Gerenciar Usuários</button></a></p>
-<?php endif; ?>
-
-<button><a href="adicionarContato.php">ADICIONAR CONTATO</a></button>
+<button><a href="adicionarContato.php">Adicionar Contato</a></button>
 
 <table border="2" width="100%">
     <thead>
@@ -41,7 +35,6 @@ $fn = new funcoes();
             <th>Data de Nascimento</th>
             <th>Foto</th>
             <th>Ativo</th>
-            <th>Ações</th>
         </tr>
     </thead>
     <tbody>
@@ -58,10 +51,6 @@ $fn = new funcoes();
                 <td><?= $fn->dtNasc($item['dtNasc'], 2); ?></td>
                 <td><?= htmlspecialchars($item['foto']); ?></td>
                 <td><?= $item['ativo'] ? 'Sim' : 'Não'; ?></td>
-                <td>
-                    <a href="editarContato.php?id=<?= $item['id']; ?>">Editar</a>
-                    <a href="excluirContato.php?id=<?= $item['id']; ?>" onclick="return confirm('Você tem certeza que deseja excluir?')">Excluir</a>
-                </td>
             </tr>
         <?php endforeach; ?>
     </tbody>
