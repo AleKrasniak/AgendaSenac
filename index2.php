011069f08c6d14f540045000d05d786a90c3ae94
AleKrasniak <142438661+AleKrasniak@users.noreply.github.com>
2025-09-17 09:24:00 -0300

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,17 +1,37 @@
-<?php 
+<?php
+session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 include 'inc/header.inc.php';
 include 'classes/contatos.class.php';
 include 'classes/funcoes.class.php';
+include 'classes/usuario.class.php';
 
+if (!isset($_SESSION['usuario'])) {
+    header('Location: login.php');
+    exit;
+}
+
+$usuario = $_SESSION['usuario'];
 $contato = new Contatos();
 $fn = new funcoes();
 ?>
 
 <h1>Agenda Senac 2025</h1>
-<button><a href="adicionarContato.php">ADICIONAR</a></button>
+
+<p>Bem-vindo, <?php echo $usuario['nome']; ?>! <a href="logout.php">Sair</a></p>
+
+<?php if (Usuario::temPermissao($usuario, 'adm')): ?>
+    <p>
+        <a href="usuarios.php"><button>Gerenciar Usuários</button></a>
+        <a href="adicionarContato.php"><button>Adicionar Contato</button></a>
+    </p>
+<?php else: ?>
+    <p>
+        <a href="agenda.php"><button>Visualizar Agenda</button></a>
+    </p>
+<?php endif; ?>
 
 <table border="2" width="100%">
     <thead>
@@ -41,10 +61,14 @@ $fn = new funcoes();
                 <td><?php echo $item['nome']; ?></td>
                 <td><?php echo $fn->dtNasc($item['dtNasc'], 2); ?></td>
                 <td><?php echo $item['foto']; ?></td>
-                <td><?php echo $item['ativo']; ?></td>
+                <td><?php echo $item['ativo'] ? 'Sim' : 'Não'; ?></td>
                 <td>
-                    <a href="editar.Contato.php?id=<?php echo $item['id']; ?>">Editar</a>
-                    <a href="excluirContato.php?id=<?php echo $item['id']; ?>"onclick="return confirm('Você tem certeza que deseja excluir??')">Excluir</a>
+                    <?php if (Usuario::temPermissao($usuario, 'adm')): ?>
+                        <a href="editarContato.php?id=<?php echo $item['id']; ?>">Editar</a>
+                        <a href="excluirContato.php?id=<?php echo $item['id']; ?>" onclick="return confirm('Você tem certeza que deseja excluir?')">Excluir</a>
+                    <?php else: ?>
+                        Apenas visualização
+                    <?php endif; ?>
                 </td>
             </tr>
         <?php endforeach; ?>
